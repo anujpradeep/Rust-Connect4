3c6d9d7f4b8f50f813ef65b1282a2d0363acd2eb
Anuj Pradeep <pradeepanuj123@gmail.com>
2025-05-19 15:25:57 -0600

    Create enems for Player Id and Player Type. implemented formatter for Player

--- connect4/src/player.rs
+++ connect4/src/player.rs
@@ -1,5 +1,28 @@
+use std::fmt;
+
 #[derive(Clone, Copy, PartialEq, Debug)]
-pub enum Player {
+pub enum PlayerId {
+    Player1,
+    Player2,
+}
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub enum PlayerType {
     Human,
     AI,
+}
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub struct Player {
+    pub id: PlayerId,
+    pub kind: PlayerType,
+}
+
+impl fmt::Display for Player {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.id {
+            PlayerId::Player1 => write!(f, "Player 1"),
+            PlayerId::Player2 => write!(f, "Player 2"),
+        }
+    }
 }
